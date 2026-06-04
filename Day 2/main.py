167951527773a5d7069c214a08cac0073bc4f707
ALSTINGLORIA <144704585+ALSTINGLORIA@users.noreply.github.com>
2026-06-04 11:21:37 +0530

    Add files via upload

---
+++ Day 2/main.py
@@ -0,0 +1,17 @@
+#This program teaches about parameters.
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return "Welcome to the home page"
+
+@app.get("/station")
+def station(station_num):
+    if station_num == 1:
+        return "Romani"
+    elif station_num == 2:
+        return "Kolarru"
+    else:
+        return "Error no such station"
+
